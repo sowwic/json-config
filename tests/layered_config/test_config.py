2718d259b20872709400559b4274d1dafe06c66f
sowwic <47799401+sowwic@users.noreply.github.com>
2026-04-09 16:13:14 +0100

    Add writing part to single layer config test

--- tests/layered_config/test_config.py
+++ tests/layered_config/test_config.py
@@ -11,7 +11,11 @@ from json_config.api import (
 )
 
 
-def test_init_with_single_layer(simple_config_file: pathlib.Path):
+def test_init_with_single_layer(
+    simple_config_file: pathlib.Path,
+    layered_config_output_dir: pathlib.Path,
+    request: pytest.FixtureRequest,
+):
     @dataclasses.dataclass
     class TestValues(ConfigValues):
         int_value: int = 0
@@ -28,3 +32,9 @@ def test_init_with_single_layer(simple_config_file: pathlib.Path):
     assert config.values.int_value == 0
     config.resolve()
     assert config.values.int_value == 5
+
+    config.values.int_value = 10
+    config.manager["root"].file_path = (
+        layered_config_output_dir / f"{request.node.name}_config.json"
+    )
+    config.save()
