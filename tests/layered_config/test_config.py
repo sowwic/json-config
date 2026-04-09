7c38454315f066db40bf0633e34d715ad1c8b717
sowwic <47799401+sowwic@users.noreply.github.com>
2026-04-09 17:09:55 +0100

    Add test_defaults_writing test for LayeredConfig

--- tests/layered_config/test_config.py
+++ tests/layered_config/test_config.py
@@ -38,3 +38,57 @@ def test_init_with_single_layer(
         layered_config_output_dir / f"{request.node.name}_config.json"
     )
     config.save()
+
+
+def test_defaults_writing(
+    layered_config_output_dir: pathlib.Path,
+    request: pytest.FixtureRequest,
+):
+    """Test that defaults are written to the root layer file when saving."""
+
+    @dataclasses.dataclass
+    class TestValues(ConfigValues):
+        int_value: int = dataclasses.field(default=0)
+        str_value: str = dataclasses.field(default="default")
+        list_value: list[str] = dataclasses.field(
+            default_factory=lambda: ["a", "b", "c"]
+        )
+
+    class TestConfig(LayeredConfig[TestValues]):
+        VALUES_CLASS = TestValues
+
+    # Setup manager
+    manager = LayeredConfigManager()
+    root_layer = ConfigLayer(
+        "root", file_path=layered_config_output_dir / f"{request.node.name}_config.json"
+    )
+    extra_root_layer = ConfigLayer(
+        "extra_root",
+        file_path=layered_config_output_dir
+        / f"{request.node.name}_extra_root_config.json",
+    )
+    extra_child_layer = ConfigLayer(
+        "extra",
+        file_path=layered_config_output_dir / f"{request.node.name}_extra_config.json",
+        depends_on=["root"],
+    )
+    manager.register(root_layer)
+    manager.register(extra_root_layer)
+    manager.register(extra_child_layer)
+    manager.load_all()
+
+    config = TestConfig(manager)
+    #! Do not run resolve so that defaults are written to the file
+    config.save()
+    assert manager["root"].file_path.is_file()
+
+    # After saving files for root layers should contain the defaults
+    manager.load_all()
+    assert (
+        root_layer.get_data()
+        == extra_root_layer.get_data()
+        == TestValues.get_defaults()
+    )
+    # The extra layer file should not be created as it's empty
+    assert extra_child_layer.get_data() == {}
+    assert not extra_child_layer.file_path.is_file()
