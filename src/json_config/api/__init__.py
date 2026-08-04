547a8f2d08c8d871876127b3687eaa214a34d6a5
sowwic <47799401+sowwic@users.noreply.github.com>
2026-08-04 08:55:39 +0100

    Got rid of SimpleConfig, move LayeredConfig and related classes under core

--- src/json_config/api/__init__.py
+++ src/json_config/api/__init__.py
@@ -1,12 +1,10 @@
-from ..layered_config.config import ConfigValues, LayeredConfig
-from ..layered_config.config_layer import ConfigLayer
-from ..layered_config.config_manager import LayeredConfigManager
-from ..simple_config import SimpleConfig
+from ..core.config import ConfigValues, LayeredConfig
+from ..core.config_layer import ConfigLayer
+from ..core.config_manager import LayeredConfigManager
 
 __all__ = [
-    "LayeredConfig",
     "ConfigLayer",
-    "LayeredConfigManager",
-    "SimpleConfig",
     "ConfigValues",
+    "LayeredConfig",
+    "LayeredConfigManager",
 ]
